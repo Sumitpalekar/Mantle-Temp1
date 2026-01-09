e70eb3a55629648567db5951804ad3c148f3a959
Palekar Sumit Namdev <23je0679@iitism.ac.in>
2026-01-09 23:38:35 +0530

    Delete src directory

--- src/interface/IVerifier.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.24;
-
-interface IVerifier {
-    function verify(bytes calldata _proof, bytes32[] calldata _publicInputs) external returns (bool);
-
-}
